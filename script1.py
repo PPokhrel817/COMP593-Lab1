b0e732e427f9ef0e19ed545817bf6f16b7f66e93
PPokhrel817 <pokhrel.prince@flemingcollege.ca>
2025-01-14 13:08:34 -0500

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("My name is Prince Pokhrel and i am from Nepal")
-print("I love travelling many places and learn different cultures")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# I love my country and the people
 print("Nepal is a very Beautiful country with beautiful places like pokhara, kathmandu and many more ")
 print("One of the best place to visit in nepal is Mustang")
