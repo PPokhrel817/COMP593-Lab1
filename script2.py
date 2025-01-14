11832865576285629213a7ee3797e2ba7d61ac00
PPokhrel817 <pokhrel.prince@flemingcollege.ca>
2025-01-14 13:03:00 -0500

    Revert "Modified both script files"
    This reverts commit e70824ec93e4f9bd2a6ca28eab1b4c01731f02b7.

--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
 print("Nepal is a very Beautiful country with beautiful places like pokhara, kathmandu and many more ")
 print("One of the best place to visit in nepal is Mustang")
-print("I love travelling many places and learn different cultures")
